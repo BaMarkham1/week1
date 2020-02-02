759a9417edbd4ecf65fa9eb020b3e1ebce76bd3b
BaMarkham1 <benjamin.lemarc@ucdenver.edu>
2020-02-02 16:34:02 -0700

    second commit

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,14 +8,32 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    //var string_results;
+    //string_results = '{"data" : '.concat(results.data, ', "status" : ', results.status, ', "statusText" : ', results.statusText, ', "headers" : ', results.headers, "}");
+    //console.log(string_results);
+    //console.log(results.data)
+    //console.log(results.headers)
+
+    var json_results = { data : results.data, status : results.status, statusText : results.statusText, headers : results.headers, requestHeader : results.config.headers};
+
+    //requestHeader: response.config.headers
+
+    //console.log(json_results)
+
+    //console.log(JSON.stringify(results));
+    //console.log(json_results.headers)
+
+    return JSON.stringify(json_results);
+
+    //return JSON.stringify(results.data);
+
 }
 
 /*  console.log(results.data);
     console.log(results.status);
     console.log(results.statusText);
     console.log(results.headers);
-
+//
         const results = await axios({
         method: 'get',
         url: 'https://www.googleapis.com/books/v1/volumes',
